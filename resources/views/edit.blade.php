18189e50b7879c95c81fd9ccb22dd61a786be0c0
Joef Resoriaga <xxjoops.0001@gmail.com>
2022-10-12 23:52:50 +0800

    remove role in edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -50,7 +50,6 @@
                             <tr>
                                 <th>Name</th>
                                 <th>Email</th>
-                                <th>Role</th>
                                 <th>Action</th>
                             </tr>
                             <form action="{{ route('admin.update') }}" method="POST">
@@ -58,20 +57,6 @@
                             {{ csrf_field() }}
                                 <td><input type="hidden" class="form-control" name="id" value="{{$users->id}}"/><input type="text" class="form-control" name="name" value="{{$users->name}}"/></td>
                                 <td><input type="email" class="form-control" name="email" value="{{$users->email}}"/></td>
-                                <td>
-                                    <div class="col-md-6">
-                                        <select name="roles" data-parsley-trigger="keyup">
-                                        @if (Auth::user()->roles == 'super admin')
-                                            <option value="admin">Admin</option>
-                                            <option value="user">User</option>
-                                        @elseif (Auth::user()->roles == 'admin')
-                                        
-                                            <option value="user">User</option>
-                                            @else
-                                        @endif
-                                        </select>
-                                    </div>
-                                </td>
                                 <td>
                                     <input type="button" name="submit" value="Update" class="btn btn-success mx-2">
                                     <a href="{{ URL::previous() }}" class="btn btn-primary"><i class="fa-solid fa-arrow-left"></i> Back</a>
